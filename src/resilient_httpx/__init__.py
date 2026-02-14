3948f9e9ca461cd4752acfddb19fdd955d887295
galthran-wq <levmorozov900@gmail.com>
2026-02-14 14:06:44 +0000

    Bump version to 0.2.0

--- src/resilient_httpx/__init__.py
+++ src/resilient_httpx/__init__.py
@@ -2,7 +2,7 @@ from resilient_httpx.client import ProxyHttpClient
 from resilient_httpx.exceptions import AllProxiesExhausted, MaxRetriesExceeded
 from resilient_httpx.retry import RetryPolicy
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "ProxyHttpClient",
